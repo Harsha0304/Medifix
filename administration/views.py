7e3500a8777647ec4ff359b239e83e3bd13a3f47
TS Tamarai Selvan <tamaraiselvan.official@gmail.com>
2024-03-05 23:00:48 +0530

    4th stage update.
    4th staging update.

--- administration/views.py
+++ administration/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from administration.models import camp_details
+import json
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    camps = camp_details.objects.all()
+    camp_data = [{'lat': camp.lattitude, 'lng': camp.longitude, 'description': camp.description} for camp in camps]
+    return render(request, 'index.html', {'camp_data': json.dumps(camp_data)})
