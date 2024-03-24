5ef00ce69024a28d453751824aa21033fe8053cd
TS Tamarai Selvan <tamaraiselvan.official@gmail.com>
2024-03-24 19:04:05 +0530

    6th staging update.
    This is 6th staging update.

--- medifix_master/urls.py
+++ medifix_master/urls.py
@@ -1,17 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LoginView
-from django.conf.urls.static import static
-from django.conf import settings
 from administration import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', LoginView.as_view(), name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('home/', views.index, name="home"),
-    path('doctors/', views.doctors, name="doctors"),
-    path('skills/', views.skills, name="skills"),
-    path('users/', views.users, name="user_detail"),
-    path('camp-detail/', views.camp_detail, name="camp_detail"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.index, name="index"),
+]
